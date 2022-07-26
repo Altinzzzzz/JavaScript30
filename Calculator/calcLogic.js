1c95f4484d5d6fa051da5496351dadae64e2dfa4
Altin <107879235+Altinzzzzz@users.noreply.github.com>
2022-07-26 08:33:53 +0200

    Add files via upload

--- Calculator/calcLogic.js
+++ Calculator/calcLogic.js
@@ -8,16 +8,17 @@ var numbers = document.querySelectorAll('.numbers');
 var operators = document.querySelectorAll('.operators');
 var point = document.querySelector('.point');
 var count = 0;
+var secondCount = 0;
 
 clear.addEventListener('click', clearText);
 
 function clearText(){
     theFirstNumber = [];
     theSecondNumber = [];
-    console.log(theFirstNumber);
-    console.log(theSecondNumber);
     operator = null;
     show.textContent = '';
+    count = 0;
+    secondCount = 0;
 }
 
 for(var i = 0; i < numbers.length; i++){
@@ -29,18 +30,68 @@ for(var i = 0; i < operators.length; i++){
     operators[i].addEventListener('click', checkOperator);
 }
 
-point.addEventListener('click', () => {
-    if(operator != null){
+point.addEventListener('click', checkOccupation);
+
+function checkPoint(){
+    if(operator == null){
+        
         if(theFirstNumber.length == 0){
             theFirstNumber.push('0');
-            checkOccupation();
+            return true;
+        }
+
+        if(theFirstNumber.includes('.')){
+            return false;
+        }
+
+        if((typeof theFirstNumber === 'string' && theFirstNumber[0].includes('.')) || (typeof theFirstNumber === 'number' && theFirstNumber[0].toString().includes('.'))){
+                return false;
+        }//after an opertion is done, the whole number is put on theFirstNumber[0]
+        // so the if above this one can't find a '.'
+        // we do this one do see whether that element in the array has a point '.'
+        //we include typeof to see whether thefirstnumber[0] exists because it throws an error if theres no element inside 
+    } else {
+        if(theSecondNumber.length == 0){
+            theSecondNumber.push('0');
+            return true;
+        }
+
+        if(theSecondNumber.includes('.')){
+            return false;
+        }
+    }
+}
+
+function checkNull(){
+    if(operator == null){
+        if(theFirstNumber.length >= 8){
+            return false;
+        }
+
+        if((typeof theFirstNumber[0] === 'string' && theFirstNumber[0].length >= 8) || (typeof theFirstNumber[0] === 'number' && theFirstNumber[0].toString().length >= 8)){
+            return false;
+        }
+
+    } else {
+        if(theSecondNumber.length >= 8){
+            return false;
+        }
+    }
+}
+
+
+function checkOccupation(){
+
+    if(checkNull() == false){
+        return;
+    }
+
+    if(this.textContent == '.'){
+        if(checkPoint() == false){
+            return;
         }
     }
 
-    checkOccupation();
-});
-
-function checkOccupation(){
     if(operator == null){
         theFirstNumber.push(this.textContent);
         show.textContent = theFirstNumber.join('');
@@ -96,32 +147,30 @@ function checkOperator(){
             operate('/');
             break;
     }
-    
-    if(count == 0){
-            show.textContent = theFirstNumber.join('');
-        if(this.textContent == '='){
-            operator = null; // if new operator is '=', then don't add it
-        } else {
-            operator = this.textContent; // if new operator is + = * /, then add it
-            show.textContent += operator;
-        }
-    } else{
+
+    if (count == 1){
         show.textContent = "Holup, that don't make sense";
-        show.style.color = 'red';
         operator = null;
         count = 0;
+        return;
+
+    } else if (secondCount == 1) {
+        show.textContent = 'The numbers are too big';
+        operator = null;
+        secondCount = 0;
+        return;
+    }
+    
+    show.textContent = theFirstNumber.join('');
+    if(this.textContent == '='){
+        operator = null; // if new operator is '=', then don't add it
+    } else {
+        operator = this.textContent; // if new operator is + - * /, then add it
+        show.textContent += operator;
     }
 }
 
 function operate(sign){
-
-    if(theFirstNumber[0] == '.'){
-        theFirstNumber.unshift('0');
-    }
-
-    if(theSecondNumber[0] == '.'){
-        theFirstNumber.unshift('0');
-    }
 
     var tempArray = +theFirstNumber.join('');
     var secTempArray = +theSecondNumber.join('');
@@ -142,6 +191,20 @@ function operate(sign){
             theFirstNumber.push(tempArray / secTempArray);
         }
     }
-    console.log(count);
+
+    if(theFirstNumber[0] % 1 != 0){
+        theFirstNumber[0] = theFirstNumber[0].toFixed(2);
+    } // if it has numbers after point '.', then only show 2 of them
+
+    if((typeof theFirstNumber[0] === 'number' && theFirstNumber[0].toString().length > 9) || (typeof theFirstNumber[0] === 'string' && theFirstNumber[0].length > 11)){
+        theFirstNumber = [];
+        secondCount++;
+    } // we check if its a number because if it is then theFirstNumber[0].length gives an error because .length is a string method
 
 } // could to this.textContent instead of sign, but this is more readable
+
+
+/* still some problems left
+    : NO BACKSPACE IMPLEMENTED YET
+    : NO KEYBOARD SUPPORT IMPLEMENTED YET
+*/
